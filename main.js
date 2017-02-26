6e474d83bb605cea1540a91cbd0c7634ef0c2525
Ruud Verheijden <ruud.verheijden@gmail.com>
2017-02-26 17:13:09 +0100

    Retrieving P1 data via the p1-reader module

--- main.js
+++ main.js
@@ -1,7 +1,52 @@
+var P1Reader = require('p1-reader');
+
 var config = require('./config/config.json');
 
-function P1Storage(options) {
+var readerConfig = {
+    emulator: true,
+    emulatorOverrides: {
+        electricityOffset: 100,
+        electricityIncrement: 0.500,
+        gasOffset: 50,
+        gasIncrement: 0.100,
+        interval: 1,
+        intervalGas: 3 // Must be larger than 'interval'
+    }
+};
 
-}
+var p1Reader = new P1Reader(readerConfig);
 
-module.exports = P1Storage;
+p1Reader.on('connected', function(port) {
+    console.log('Connection with the Smart Meter has been established on port: ' + port);
+});
+
+p1Reader.on('reading', function(data) {
+    console.log('Reading received: currently consuming ' + data.electricity.received.actual.reading + data.electricity.received.actual.unit);
+
+    // Write electricity totals and actual value to CSV
+    var output = '' +
+        data.timestamp + ';' +
+        data.electricity.received.tariff1.reading + ';' +
+        data.electricity.received.tariff2.reading + ';' +
+        data.electricity.received.actual.reading + ';' +
+        data.electricity.tariffIndicator + ';' +
+        data.electricity.numberOfPowerFailures + ';' +
+        data.electricity.numberOfLongPowerFailures + ';' +
+        data.gas.timestamp + ';' +
+        data.gas.reading + '\n';
+
+    console.log(output);
+});
+
+p1Reader.on('error', function(error) {
+    console.log(error);
+});
+
+p1Reader.on('close', function() {
+    console.log('Connection closed');
+});
+
+// Handle all uncaught errors without crashing
+process.on('uncaughtException', function(error) {
+    console.error(error);
+});
